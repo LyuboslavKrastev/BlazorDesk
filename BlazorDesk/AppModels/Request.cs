f924d9126aa5352ae983d6b102f61f1cc6aebf92
Lyuboslav Krastev <krastevlyuboslav@gmail.com>
2019-09-06 22:14:41 +0300

    Initial Commit
    Checking out Blazor.

---
+++ BlazorDesk/AppModels/Request.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorDesk.AppModels
+{
+    public class Request
+    {
+        public int Id { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ BlazorDesk/Server/Controllers/RequestsController.cs
@@ -0,0 +1,22 @@
+﻿using BlazorDesk.AppModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorDesk.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RequestsController : ControllerBase
+    {
+        [HttpGet]
+        public Request Get()
+        {
+            var request = new Request
+            {
+                Id = 1,
+                Subject = "Subject 1",
+                Description = "Description!"
+            };
+            return request;
+        }
+    }
+}
